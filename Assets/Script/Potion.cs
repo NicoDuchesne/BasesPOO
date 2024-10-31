0b50a9f27c57c0824d5c169104714fb016d65b2e
NicoDuchesne <nicolas.duchesne@edu.devinci.fr>
2024-10-31 17:31:54 +0100

    [ADD] editor and GD friendly features

--- Assets/Script/Potion.cs
+++ Assets/Script/Potion.cs
@@ -1,9 +1,18 @@
+using NaughtyAttributes;
 using UnityEngine;
 
 public class Potion : Item
 {
     [SerializeField] Health _health;
-    [SerializeField] private int _regen;
+    [SerializeField, ValidateInput(nameof(ValidateRegen), "_regen must be beetween 1 and 100 included")]
+    private int _regen;
+
+    private bool ValidateRegen() => _regen > 0 && _regen <= 100;
+
+    private void Reset()
+    {
+        _regen = 25;
+    }
 
     protected override void Effect()
     {
